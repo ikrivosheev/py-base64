512cd89be69e2e134eff55c103e030247cc93852
Ivan Krivosheev <py.krivosheev@gmail.com>
2020-04-23 01:13:09 +0300

    Add travis config

--- tests/test_decode.py
+++ tests/test_decode.py
@@ -3,3 +3,8 @@ from b64_stream import Base64StreamDecode
 
 def test_noop():
     decoder = Base64StreamDecode()
+    print([chunk for chunk in decoder.update(b'MTIzNAo=')])
+    decoder.finalize()
+    print(decoder.total)
+
+test_noop()
