87f0dd962a08e810cf4ee3118277db100781ba01
Ivan Krivosheev <py.krivosheev@gmail.com>
2020-06-08 01:41:02 +0300

    Fix

--- b64_stream/__init__.py
+++ b64_stream/__init__.py
@@ -1,4 +1,4 @@
-from ._b64_stream import Base64StreamDecode
-from ._b64_stream import Base64StreamEncode
+from b64_stream._b64_stream import Base64StreamDecode
+from b64_stream._b64_stream import Base64StreamEncode
 
 __all__ = ('Base64StreamDecode', 'Base64StreamEncode')
