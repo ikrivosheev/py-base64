d4a422e198f4a08179facef24f067ffadde0452c
Ivan Krivosheev <py.krivosheev@gmail.com>
2020-06-11 00:04:57 +0300

    Add base class

--- b64_stream/_b64_stream.pyx
+++ b64_stream/_b64_stream.pyx
@@ -13,26 +13,52 @@ def chunks(data, count):
         yield data[i:i+count]
 
 
+cdef class BufferedBase64Stream:
+    cdef char* _buffer
+    cdef size_t _chunk_size
 
-cdef class Base64StreamDecode:
-    cdef char* _buffer;
-    cdef size_t _chunk_size;
+    def __init__(self, size_t buffer_size = 2 ** 10):
+        self._buffer = <char*> malloc(sizeof(char) * buffer_size)
+        if self._buffer is NULL:
+            raise MemoryError()
+    
+    @property
+    def chunk_size(self):
+        return self._chunk_size
+    
+    @property
+    def total(self):
+        raise NotImplementedError
+
+    def clear(self):
+        raise NotImplementedError
+
+    def _process_chunk(self, chunk):
+        raise NotImplementedError
+
+    def finalize(self):
+        raise NotImplementedError
+
+    def update(self, data):
+        with memoryview(data) as mem:
+            for chunk in chunks(mem, self._chunk_size):
+                yield self._process_chunk(chunk)
+
+    def __dealloc__(self):
+        free(self._buffer)
+
+
+cdef class Base64StreamDecode(BufferedBase64Stream):
     cdef _b64_stream.b64_decode_state* _c_state
 
-    def __cinit__(self, size_t buffer_size = 2 ** 10):
+    def __init__(self, size_t buffer_size = 2 ** 10):
+        super().__init__(buffer_size)
         self._c_state = <_b64_stream.b64_decode_state*> malloc(sizeof(_b64_stream.b64_decode_state))
         if self._c_state is NULL:
             raise MemoryError()
         _b64_stream.b64_stream_decode_init(self._c_state)
-
-        self._buffer = <char*> malloc(sizeof(char) * buffer_size)
-        if self._buffer is NULL:
-            raise MemoryError()
+        
         self._chunk_size = buffer_size * 4 // 3
-
-    @property
-    def chunk_size(self):
-        return self._chunk_size
 
     @property
     def total(self):
@@ -41,21 +67,18 @@ cdef class Base64StreamDecode:
     def clear(self):
         _b64_stream.b64_stream_decode_init(self._c_state)
 
-    def update(self, data):
+    def _process_chunk(self, chunk):
         cdef size_t length
-        cdef Py_buffer* chunk_buffer
-        
-        with memoryview(data) as mem:
-            for chunk in chunks(mem, self._chunk_size):
-                chunk_buffer = PyMemoryView_GET_BUFFER(chunk)
-                length = _b64_stream.b64_stream_decode(
-                    self._c_state, 
-                    <const char*>(chunk_buffer.buf), 
-                    len(chunk), 
-                    self._buffer)
+        cdef Py_buffer* chunk_buffer = PyMemoryView_GET_BUFFER(chunk)
 
-                yield self._buffer[:length]        
+        length = _b64_stream.b64_stream_decode(
+            self._c_state, 
+            <char*> chunk_buffer.buf, 
+            len(chunk), 
+            self._buffer)
 
+        return self._buffer[:length]
+    
     def finalize(self):
         result = _b64_stream.b64_stream_decode_final(self._c_state)
         if not result:
@@ -63,50 +86,38 @@ cdef class Base64StreamDecode:
     
     def __dealloc__(self):
         free(self._c_state)
-        free(self._buffer)
 
 
-cdef class Base64StreamEncode:
-    cdef char* _buffer
-    cdef size_t _chunk_size
+cdef class Base64StreamEncode(BufferedBase64Stream):
     cdef _b64_stream.b64_encode_state* _c_state
 
-    def __cinit__(self, size_t buffer_size = 2 ** 10):
+    def __init__(self, size_t buffer_size = 2 ** 10):
+        super().__init__(buffer_size)
         self._c_state = <_b64_stream.b64_encode_state*> malloc(sizeof(_b64_stream.b64_encode_state))
         if self._c_state is NULL:
             raise MemoryError()
         _b64_stream.b64_stream_encode_init(self._c_state)
         
-        self._buffer = <char*> malloc(sizeof(char) * buffer_size)
-        if self._buffer is NULL:
-            raise MemoryError()
         self._chunk_size = buffer_size * 3 // 4
     
     @property
     def total(self):
         return self._c_state.out_len
     
-    @property
-    def chunk_size(self):
-        return self._chunk_size
-
     def clear(self):
         _b64_stream.b64_stream_encode_init(self._c_state)
-
-    def update(self, data):
+    
+    def _process_chunk(self, chunk):
         cdef size_t length
-        cdef Py_buffer* chunk_buffer
+        cdef Py_buffer* chunk_buffer = PyMemoryView_GET_BUFFER(chunk)
 
-        with memoryview(data) as mem:
-            for chunk in chunks(mem, self._chunk_size):
-                chunk_buffer = PyMemoryView_GET_BUFFER(chunk)
-                length = _b64_stream.b64_stream_encode(
-                    self._c_state, 
-                    <const char*>(chunk_buffer.buf), 
-                    len(chunk), 
-                    self._buffer)
+        length = _b64_stream.b64_stream_encode(
+            self._c_state, 
+            <char*> chunk_buffer.buf, 
+            len(chunk), 
+            self._buffer)
+        return self._buffer[:length]
 
-                yield self._buffer[:length]        
 
     def finalize(self):
         cdef size_t length
@@ -119,4 +130,3 @@ cdef class Base64StreamEncode:
 
     def __dealloc__(self):
         free(self._c_state)
-        free(self._buffer)
