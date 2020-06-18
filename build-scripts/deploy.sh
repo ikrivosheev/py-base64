d85773263dfef9e4fca063198c796fd1bfd37de2
Ivan Krivosheev <py.krivosheev@gmail.com>
2020-06-19 01:12:33 +0300

    Fix deploy

--- build-scripts/deploy.sh
+++ build-scripts/deploy.sh
@@ -2,16 +2,14 @@
 
 set -e
 
-wheelhouse="/tmp/wheel/"
-
 pip install twine wheel auditwheel
-pip wheel . --wheel-dir "$wheelhouse"
+python setup.py sdist bdist_wheel
 
 # Bundle external shared libraries into the wheels.
-for whl in "$wheelhouse"/*; do
-    auditwheel repair --plat="manylinux2014_x86_64" $whl -w "$wheelhouse"
-    rm "$wheelhouse"/*-linux_*.whl
+for whl in dist/*.whl; do
+    auditwheel repair --plat="manylinux2014_x86_64" $whl -w dist/
+    rm dist/*-linux_*.whl
 done
 
-python setup.py sdist
 twine upload dist/*.whl dist/*.tar.*
+
